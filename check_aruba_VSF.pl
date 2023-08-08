c68895856e24b3a4d4616022091f34fced3c8f1e
oxame <37661797+oxame@users.noreply.github.com>
2023-08-08 15:49:46 +0200

    Create check_aruba_VSF.pl

---
+++ check_aruba_VSF.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+##################################################################
+# Creation: Skrabal Arnault
+# Last Modification:
+# 
+# Test intégité d'un stack VSF
+#
+##################################################################
+
+
+use strict;
+use warnings;
+
+
+
+my $OID_VSFTATEMENBER = 'HP-VSF-VC-MIB::hpicfVsfVCMemberState';
+
+my $STATE_OK = 0;
+my $STATE_WARNING = 1;
+my $STATE_CRITICAL = 2;
+my $STATE_UNKNOWN = 3;
+my $STATE_DEPENDENT = 4;
+my $STATE = 3;
+
+
+my $Command = "snmpwalk -v $ARGV[0] -c $ARGV[1] $ARGV[2] $OID_VSFTATEMENBER";
+my $result = `$Command`;
+#$result =~ m/IF-MIB::ifDescr\.(\d+)\s=\s.*$interface.*/;
+my $Menber = -1;
+my $MenberMissing = 0;
+foreach(split("HP-VSF-VC-MIB::hpicfVsfVCMemberState.",$result))
+{
+$Menber++;
+	if($_ =~ "missing")
+	{
+		$MenberMissing = $Menber;
+	}
+}
+
+if($MenberMissing != 0)
+{
+	print "Stack ERROR Menber $MenberMissing is MISSING";
+	$STATE = $STATE_CRITICAL;
+}
+else
+{
+	print "Stack $Menber menber";
+	$STATE = $STATE_OK;
+}
+
+exit $STATE;
